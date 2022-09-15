f82967d9c5cd6a4ef8d7755fc6c9b954589fd03e
Ahmad AlBashash <ahmad.bsh237@gmail.com>
2022-09-16 00:10:14 +0300

    -added Events Section

--- index-main.php
+++ index-main.php
@@ -1,224 +1,306 @@
  <?php
-session_start();
-if (isset($_SESSION['email'])) {
-?> 
-<!DOCTYPE html>
-<html lang="en">
+  session_start();
+  if (isset($_SESSION['email'])) {
+  ?>
+   <!DOCTYPE html>
+   <html lang="en">
 
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <head>
+     <meta charset="UTF-8">
+     <meta http-equiv="X-UA-Compatible" content="IE=edge">
 
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-  <title>Restaurantly</title>
+     <title>Restaurantly</title>
 
-  <!-- Main Css File -->
-  <link rel="stylesheet" href="./assets/css/style.css">
+     <!-- Main Css File -->
+     <link rel="stylesheet" href="./assets/css/style.css">
 
-  <!-- BootStrap Files -->
-  <link href="assets/css/bootstrap.min.css" rel="stylesheet">
-  <link href="assets/css/bootstrap-icons.css" rel="stylesheet">
+     <!-- Slick Css Files -->
+     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick-theme.min.css">
+     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.css">
 
-
-
-</head>
-
-<body>
+     <!-- BootStrap Files -->
+     <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+     <link href="assets/css/bootstrap-icons.css" rel="stylesheet">
 
 
-  
-  <!-- navbar -->
-  <div> </div>
+
+   </head>
+
+   <body>
 
 
-  <!-- Home -->
-  <div> </div>
+
+     <!-- navbar -->
+     <div> </div>
 
 
-  <!-- About -->
-  <div>
-  <section  class="about">
-      <div class="container" data-aos="fade-up">
-
-        <div class="row">
-          <div class="col-lg-6 order-1 order-lg-2" >
-            <div class="about-img">
-              <img src="assets/img/about.jpg" alt="">
-            </div>
-          </div>
-          <div class="col-lg-6 pt-4 pt-lg-0 sidenav content">
-            <h3>Voluptatem dignissimos provident quasi corporis voluptates sit assumenda.</h3>
-            <p class="fst-italic">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
-              magna aliqua.
-            </p>
-            <ul class="ul">
-              <li><i class="bi bi-1-square-fill"></i> Ullamco laboris nisi ut aliquip ex ea commodo consequat.</li>
-              <li><i class="bi bi-2-square-fill"></i> Duis aute irure dolor in reprehenderit in voluptate velit.</li>
-              <li><i class="bi bi-3-square-fill"></i> Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate trideta storacalaperda mastiro dolore eu fugiat nulla pariatur.</li>
-            </ul>
-            <p>
-              Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-              velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-              culpa qui officia deserunt mollit anim id est laborum
-            </p>
-          </div>
-        </div>
-
-      </div>
-    </section>
+     <!-- Home -->
+     <div> </div>
 
 
-    <section class="whyus">
-      <div class="container" data-aos="fade-down">
+     <!-- About -->
+     <div>
+       <section class="about">
+         <div class="container" data-aos="fade-up">
 
-        <div class="title">
-          <h2 class="h2">Why Us</h2>
-          <p class="p">Why Choose Our Restaurant ?!</p>
-        </div>
+           <div class="row">
+             <div class="col-lg-6 order-1 order-lg-2">
+               <div class="about-img">
+                 <img src="assets/img/about.jpg" alt="">
+               </div>
+             </div>
+             <div class="col-lg-6 pt-4 pt-lg-0 sidenav content">
+               <h3>Voluptatem dignissimos provident quasi corporis voluptates sit assumenda.</h3>
+               <p class="fst-italic">
+                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+                 magna aliqua.
+               </p>
+               <ul class="ul">
+                 <li><i class="bi bi-1-square-fill"></i> Ullamco laboris nisi ut aliquip ex ea commodo consequat.</li>
+                 <li><i class="bi bi-2-square-fill"></i> Duis aute irure dolor in reprehenderit in voluptate velit.</li>
+                 <li><i class="bi bi-3-square-fill"></i> Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate trideta storacalaperda mastiro dolore eu fugiat nulla pariatur.</li>
+               </ul>
+               <p>
+                 Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+                 velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+                 culpa qui officia deserunt mollit anim id est laborum
+               </p>
+             </div>
+           </div>
 
-        <div class="row">
+         </div>
+       </section>
 
-          <div class="col-lg-4">
-            <div class="square" class=" bi bi-zoom-in" >
-              <span class="span" > <i class="bi bi-chat-heart  "></i> </span>
-              <h3>Lorem Ipsum</h3>
-              <p class="p"> Ulamco laboris nisi ut aliquip ex ea commodo consequat. Et consectetur ducimus vero placeat</p>
-            </div>
-          </div>
 
-          <div class="col-lg-4 mt-4 mt-lg-0">
-            <div class="square" class=" bi bi-zoom-in" >
-              <span class="span"><i class="bi bi-chat-heart  "></i></span>
-              <h3 class="h3">Repellat Nihil</h3>
-              <p class="p">Dolorem est fugiat occaecati voluptate velit esse. Dicta veritatis dolor quod et vel dire leno para dest</p>
-            </div>
-          </div>
+       <section class="whyus">
+         <div class="container" data-aos="fade-down">
 
-          <div class="col-lg-4 mt-4 mt-lg-0">
-            <div class="square" class=" bi bi-zoom-out" >
-              <span class="span"><i class="bi bi-chat-heart "></i></span>
-              <h3 class="h3"> Ad ad velit qui</h3>
-              <p class="p">Molestiae officiis omnis illo asperiores. Aut doloribus vitae sunt debitis quo vel nam quis</p>
-            </div>
-          </div>
+           <div class="title">
+             <h2 class="h2">Why Us</h2>
+             <p class="p">Why Choose Our Restaurant ?!</p>
+           </div>
 
-        </div>
+           <div class="row">
 
-      </div>
-    </section>
+             <div class="col-lg-4">
+               <div class="square" class=" bi bi-zoom-in">
+                 <span class="span"> <i class="bi bi-chat-heart  "></i> </span>
+                 <h3>Lorem Ipsum</h3>
+                 <p class="p"> Ulamco laboris nisi ut aliquip ex ea commodo consequat. Et consectetur ducimus vero placeat</p>
+               </div>
+             </div>
+
+             <div class="col-lg-4 mt-4 mt-lg-0">
+               <div class="square" class=" bi bi-zoom-in">
+                 <span class="span"><i class="bi bi-chat-heart  "></i></span>
+                 <h3 class="h3">Repellat Nihil</h3>
+                 <p class="p">Dolorem est fugiat occaecati voluptate velit esse. Dicta veritatis dolor quod et vel dire leno para dest</p>
+               </div>
+             </div>
+
+             <div class="col-lg-4 mt-4 mt-lg-0">
+               <div class="square" class=" bi bi-zoom-out">
+                 <span class="span"><i class="bi bi-chat-heart "></i></span>
+                 <h3 class="h3"> Ad ad velit qui</h3>
+                 <p class="p">Molestiae officiis omnis illo asperiores. Aut doloribus vitae sunt debitis quo vel nam quis</p>
+               </div>
+             </div>
+
+           </div>
+
+         </div>
+       </section>
      </div>
 
 
-  <!-- Menu -->
-  <div> </div>
+     <!-- Menu -->
+     <div> </div>
 
 
-  <!-- Specials -->
-  <div> </div>
+     <!-- Specials -->
+     <div> </div>
 
 
-  <!-- Events -->
-  <div> </div>
+     <!-- Events -->
+     <div class="main-div">
+       <br>
+       <div class="event-title">EVENTS</div>
+       <h1 class="event-name">Organize Your Events in our Restaurant</h1>
+       <div class="fade-in-image">
+         <ul class="slider">
+           <div class="event-main">
+             <div class="event-left"><img src="assets/img/event-custom.jpg"></div>
+             <div class="event-right">
+               <h1>Custom Parties</h1>
+               <h2><span>$99</span></h2>
+               <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                 eiusmod tempor incididunt ut lobore et dolore magna aliqua.
+               </p>
+               <p>
+                 Ullamco laboris nisi ut aliquip ex ea commodo consequat.
+               </p>
+               <p>
+                 Duls aute irure dolor in reprehenderit in voluptate velit.
+               </p>
+               <p>
+                 ullamco laboris nisi ut aliquip ex ea commodo consequat.
+               </p>
+               <p>
+                 Ullamco laboris nisi ut aliquip ex ea commodo consequat.
+               </p>
+               <p>
+                 Duis aute irure dolor in reprehenderit in voluptate velit esse
+                 cillum dolore eu fugiat nulla pariatur.</p>
+             </div>
+           </div>
+           <div class="event-main">
+             <div class="event-left"><img src="assets/img/event-birthday.jpg"></div>
+             <div class="event-right">
+               <h1>Birthday Parties</h1>
+               <h2><span>$189</span></h2>
+               <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit,sed doeiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+               <p>Ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+               <p>Duis aute irure dolor in reprehenderit in voluptate velit.</p>
+               <p> Ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+               <p>Ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+               <p>
+                 Duis aute irure dolor in reprehenderit in voluptate velit esse
+                 cillum dolore eu fugiat nulla pariatur.</p>
+             </div>
+           </div>
+           <div class="event-main">
+             <div class="event-left"><img src="assets/img/event-private.jpg"></div>
+             <div class="event-right">
+               <h1>Private Parties</h1>
+               <h2><span>$290</span></h2>
+               <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+                 ut labore et dolore magna aliqua.</p>
+               <p> ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+               <p> Duis aute irure dolor in reprehenderit in voluptate velit.</p>
+               <p> Ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+               <p>
+                 Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolorin reprehenderit in voluptate velit esse cillurm dolore eu fugiat nulla pariatur.
+               </p>
+             </div>
+           </div>
+         </ul>
+       </div>
+     </div>
+     <script src="https://code.jquery.com/jquery-3.6.1.min.js"></script>
+     <script src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.js"></script>
+     <script type="text/javascript">
+       $('.slider').slick({
+         infinite: true,
+         dots: true,
+         arrows: false,
+         slidesToShow: 1,
+         slidesToScroll: 1,
+         //autoplay:true,
+         //autoplayspeed:5000,
+       });
+     </script>
 
 
-  <!-- Gallery -->
-  <div> </div>
+     <!-- Gallery -->
+     <div> </div>
 
 
-  <!-- Chefs -->
-  <div><section id="chefs" class="chefs">
-      <div class="container" data-aos="fade-up">
-  
-        <div class="section-title">
-          <p class="cp"><b>CHEFS</b>
-        <i class="bi bi-cup-straw"></i>
-        <i class="bi bi-cup-hot-fill"></i>
-          </p>
-          <h2 class="h2">Our Proffesional Chefs</h2>
-        </div>
-  
-        <div class="row">
-          <div class="col-lg-4 col-md-6">
-            <div class="img" data-aos="zoom-in" >
-              <img src="assets/img/chefs/chefs-1.jpg" class="img-fluid" alt="">
-              <div class="img-content">
-                <div class="content">
-                  <h4 class="h4">Walter White</h4>
-                  <span class="span">Master Chef</span>
-                </div>
-                <div class="media">
-                 <i class="bi bi-twitter"></i>
-                  <i class="bi bi-facebook"></i>
-                 <i class="bi bi-instagram"></i>
-              <i class="bi bi-linkedin"></i>
-              <i class="bi bi-whatsapp"></i>
-                </div>
-              </div>
-            </div>
-          </div>
-  
-          <div class="col-lg-4 col-md-6">
-            <div class="img" data-aos="zoom-in" >
-              <img src="assets/img/chefs/chefs-2.jpg" class="img-fluid" alt="">
-              <div class="img-content">
-                <div class="content">
-                  <h4 class="h4">Sarah Jhonson</h4>
-                  <span class="span">Patissier</span>
-                </div>
-                <div class="media">
-                  <i class="bi bi-twitter"></i>
-                  <i class="bi bi-facebook"></i>
-                 <i class="bi bi-instagram"></i>
-              <i class="bi bi-linkedin"></i>
-              <i class="bi bi-whatsapp"></i>
-                </div>
-              </div>
-            </div>
-          </div>
-  
-          <div class="col-lg-4 col-md-6">
-            <div class="img" data-aos="zoom-in">
-              <img src="assets/img/chefs/chefs-3.jpg" class="img-fluid" alt="">
-              <div class="img-content">
-                <div class="content">
-                  <h4 class="h4">William Anderson</h4>
-                  <span class="span">Cook</span>
-                </div>
-                <div class="media">
-                  <i class="bi bi-twitter"></i>
-                  <i class="bi bi-facebook"></i>
-                 <i class="bi bi-instagram"></i>
-              <i class="bi bi-linkedin"></i>
-              <i class="bi bi-whatsapp"></i>
-                </div>
-              </div>
-            </div>
-          </div>
-  
-        </div>
-  
-      </div>
-    </section> </div>
+     <!-- Chefs -->
+     <div>
+       <section id="chefs" class="chefs">
+         <div class="container" data-aos="fade-up">
+
+           <div class="section-title">
+             <p class="cp"><b>CHEFS</b>
+               <i class="bi bi-cup-straw"></i>
+               <i class="bi bi-cup-hot-fill"></i>
+             </p>
+             <h2 class="h2">Our Proffesional Chefs</h2>
+           </div>
+
+           <div class="row">
+             <div class="col-lg-4 col-md-6">
+               <div class="img" data-aos="zoom-in">
+                 <img src="assets/img/chefs/chefs-1.jpg" class="img-fluid" alt="">
+                 <div class="img-content">
+                   <div class="content">
+                     <h4 class="h4">Walter White</h4>
+                     <span class="span">Master Chef</span>
+                   </div>
+                   <div class="media">
+                     <i class="bi bi-twitter"></i>
+                     <i class="bi bi-facebook"></i>
+                     <i class="bi bi-instagram"></i>
+                     <i class="bi bi-linkedin"></i>
+                     <i class="bi bi-whatsapp"></i>
+                   </div>
+                 </div>
+               </div>
+             </div>
+
+             <div class="col-lg-4 col-md-6">
+               <div class="img" data-aos="zoom-in">
+                 <img src="assets/img/chefs/chefs-2.jpg" class="img-fluid" alt="">
+                 <div class="img-content">
+                   <div class="content">
+                     <h4 class="h4">Sarah Jhonson</h4>
+                     <span class="span">Patissier</span>
+                   </div>
+                   <div class="media">
+                     <i class="bi bi-twitter"></i>
+                     <i class="bi bi-facebook"></i>
+                     <i class="bi bi-instagram"></i>
+                     <i class="bi bi-linkedin"></i>
+                     <i class="bi bi-whatsapp"></i>
+                   </div>
+                 </div>
+               </div>
+             </div>
+
+             <div class="col-lg-4 col-md-6">
+               <div class="img" data-aos="zoom-in">
+                 <img src="assets/img/chefs/chefs-3.jpg" class="img-fluid" alt="">
+                 <div class="img-content">
+                   <div class="content">
+                     <h4 class="h4">William Anderson</h4>
+                     <span class="span">Cook</span>
+                   </div>
+                   <div class="media">
+                     <i class="bi bi-twitter"></i>
+                     <i class="bi bi-facebook"></i>
+                     <i class="bi bi-instagram"></i>
+                     <i class="bi bi-linkedin"></i>
+                     <i class="bi bi-whatsapp"></i>
+                   </div>
+                 </div>
+               </div>
+             </div>
+
+           </div>
+
+         </div>
+       </section>
+     </div>
 
 
-  <!-- Contact us -->
-  <div> </div>
+     <!-- Contact us -->
+     <div> </div>
 
 
 
-  <!-- Footer -->
-  <div> </div>
+     <!-- Footer -->
+     <div> </div>
 
 
-</body>
+   </body>
 
-</html>
+   </html>
 
  <?php
-} else {
+  } else {
     header("Location: index.php");
     exit();
-}
-?> 
+  }
+  ?>
